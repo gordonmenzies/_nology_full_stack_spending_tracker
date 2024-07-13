22b2b7a77a53de848b7c2de162d3c7e4aa636ce9
gordonmenzies <heymish96@gmail.com>
2024-07-13 10:47:49 +0100

    attempting to connect app to firebase storage

--- spending_tracker/src/App.tsx
+++ spending_tracker/src/App.tsx
@@ -5,7 +5,38 @@ import AddSpend from "./Containers/AddSpend/AddSpend";
 import Analytics from "./Containers/Analytics/Analytics";
 import "./App.css";
 
-function App() {
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZzJFtpB1asMpp6ZFow-1D52ucP-jsEH4",
+  authDomain: "spending-tracker-584a8.firebaseapp.com",
+  projectId: "spending-tracker-584a8",
+  storageBucket: "spending-tracker-584a8.appspot.com",
+  messagingSenderId: "351908282910",
+  appId: "1:351908282910:web:cb9b93634596da6d7de40f",
+  measurementId: "G-5S0XFW511L",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+// import { collection, addDoc } from "firebase/firestore";
+
+// try {
+//   const docRef = await addDoc(collection(db, "users"), {
+//     first: "Ada",
+//     last: "Lovelace",
+//     born: 1815,
+//   });
+//   console.log("Document written with ID: ", docRef.id);
+// } catch (e) {
+//   console.error("Error adding document: ", e);
+// }
+
+const App = () => {
   return (
     <div>
       <Routes>
@@ -17,6 +48,6 @@ function App() {
       <p>hello</p>
     </div>
   );
-}
+};
 
 export default App;
